7483fdee0c08dc93fae03856a9bb6cde5d11b4c0
Noor Albayaty <noor.albayaty90@gmail.com>
2021-01-25 20:31:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ return `Welcome, ${name} You are number ${line.length} in line.`;
      return "The line is currently empty.";
    }
        const now=[];
-     for(let i =0;i<line.length();i++)
+     for(let i =0, k=line.length;i<k;i++)
      {
        now.push(`${i+1}.line[i]`);
      }
