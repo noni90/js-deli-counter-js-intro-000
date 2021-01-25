d84b21073a48b54d9fb881c6869721622d3d5ea5
Noor Albayaty <noor.albayaty90@gmail.com>
2021-01-25 20:25:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,21 @@ line.push(name);
    else {
      return `Currently serving ${line.shift()}`;
    }
+}
 
+ function currentLine(line)
+ {
+   if (!line.length)
+   {
+     return "The line is currently empty.";
+   }
+   else {
+     var now=[];
+     for(let i =0;i<line.length;i++)
+     {
+       now.push(${i+1}.line[i]);
+     }
+    }
+    return `The line is currently:${now}`;
  }
+
