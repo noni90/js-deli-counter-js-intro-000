25cc87372fba15975bb9083f3a29cbd79d8c626d
Noor Albayaty <noor.albayaty90@gmail.com>
2021-01-25 20:16:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ line.push(name);
 
   return `Welcome, ${name} You are number ${line.length} in line.`;
 }
- function nowServing(katzDeliLine){
+ function nowServing(line){
+   if(!line.length)
+   {
+     return "There is nobody waiting to be served!";
+   }
    return katzDeliLine.shift();
  }
